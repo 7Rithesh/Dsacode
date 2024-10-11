71418dade93546bf7a00a60a3e4c4056402191fe
RITHESH <115831246+7Rithesh@users.noreply.github.com>
2024-10-11 23:58:33 +0530

    637. Create Average of Levels in Binary Tree[EASY]

---
+++ leetcodeQuestion/Average of Levels in Binary Tree.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<Double> averageOfLevels(TreeNode root) {
+        List<Double> list = new ArrayList<>();
+        if(root == null){
+            return list;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while(!queue.isEmpty()){
+            int levelSize = queue.size();
+            double averageSize = 0;
+            for(int i = 0 ; i < levelSize; i++){
+                TreeNode currentNode = queue.poll();
+                averageSize += currentNode.val;
+                if(currentNode.left != null){
+                    queue.offer(currentNode.left);
+                }
+                if(currentNode.right != null){
+                    queue.offer(currentNode.right);
+                }
+            }
+            averageSize = averageSize/levelSize;
+            list.add(averageSize);
+            
+        }
+        return list;
+    }
+}
